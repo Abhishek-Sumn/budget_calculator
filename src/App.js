aea8f8c7644a59dc316049b106756548b8a55773
Abhishek suman <abhisheksumancode@gmail.com>
2023-07-06 22:40:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 import { GlobalProvider } from './Context/GlobalState';
-//10
+//11
 import './App.css';
 
 function App() {
